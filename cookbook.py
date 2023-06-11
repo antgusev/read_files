384c1f192dbd9de5d7824befdc6227a50252a81a
Anton Gusev <antgusev@yandex.ru>
2023-06-11 22:17:35 +0500

    add task 3 join_files

--- cookbook.py
+++ cookbook.py
@@ -1,3 +1,5 @@
+#Задача 1
+
 import json
 
 with open('recipes.txt', 'r', encoding='utf-8') as file:
@@ -17,6 +19,9 @@ with open('recipes.txt', 'r', encoding='utf-8') as file:
     res = json.dumps(cook_book, ensure_ascii=False, indent=2)
     #print(res)
 
+
+#Задача 2
+
 def get_shop_list_by_dishes(dishes, person_count):
     ingredient_dict = {}
     for person_dish in dishes:
@@ -33,5 +38,36 @@ def get_shop_list_by_dishes(dishes, person_count):
     return ingredient_dict
                 
 
-print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+#print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+
+
+#Задача 3
+
+def join_files(files):
+    
+    count_str_list = []
+    for some_file in files:
+        with open(some_file, 'r', encoding='utf-8') as f_1:
+            count_str_list.append(len(f_1.readlines()))
+    count_str = sorted(count_str_list)
+    #print(count_str)
+
+    for some_file in files:
+        count = 0
+        with open(some_file, 'r', encoding='utf-8') as f_1:
+            res_1 = f_1.readlines()
+            if len(res_1) == count_str[count]:
+                f = open('1_3.txt', 'a', encoding='utf-8')
+                f.write(some_file + '\n')
+                f.write(str(len(res_1)) + '\n')
+                f.writelines(res_1)
+                f.close()
+            #elif count >= len(files):
+                #break
+            else:
+                count +=1
+            print(res_1)
+
+
+join_files(['1.txt', '2.txt', '3.txt'])
 
